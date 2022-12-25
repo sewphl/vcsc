0aec4d58a4361cda9404791ff97f840f087fc93e
sewphl <37678331+sewphl@users.noreply.github.com>
2022-12-24 20:59:39 -0500

    first commit

---
+++ vcsc/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7lc3-i%gg^b^t-7bkv!&#q6ez)dza#-q_92+k9ek!+q3x-q*!f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
