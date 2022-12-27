c163fa75a71547e699a15f616743d3f5c68be570
sewphl <37678331+sewphl@users.noreply.github.com>
2022-12-27 14:15:20 -0500

    install apps for routable pages (research); update research listing template

--- vcsc/settings/base.py
+++ vcsc/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "wagtail.contrib.modeladmin",
     "wagtail.contrib.redirects",
     "wagtail.contrib.settings",
+    "wagtail.contrib.routable_page",
     "wagtail.embeds",
     "wagtail.sites",
     "wagtail.users",
