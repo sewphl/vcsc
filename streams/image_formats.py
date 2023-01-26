bf0f768faade8c6cf84d31e3b207f22439668ec5
sewphl <37678331+sewphl@users.noreply.github.com>
2023-01-26 05:59:17 -0500

    create image_formats.py in streams app to make rich text images responsive; add button for nav collapse

---
+++ streams/image_formats.py
@@ -0,0 +1,12 @@
+from wagtail.images.formats import Format, register_image_format, unregister_image_format
+
+##Note, update img-responsive to img-fluid 
+
+unregister_image_format('fullwidth')
+register_image_format(Format('fullwidth', 'Full width', 'richtext-image img-fluid full-width', 'width-800'))
+
+unregister_image_format('left')
+register_image_format(Format('left', 'Left-aligned', 'richtext-image img-fluid left', 'width-500'))  
+
+unregister_image_format('right')
+register_image_format(Format('right', 'Right-aligned', 'richtext-image img-fluid right', 'width-500'))
