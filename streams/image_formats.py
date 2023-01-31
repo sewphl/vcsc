bdda37318784edaf08d84973c1d63972fad64cc3
sewphl <37678331+sewphl@users.noreply.github.com>
2023-01-31 07:07:17 -0500

    update footer button style; update streams image_formats.py

--- streams/image_formats.py
+++ streams/image_formats.py
@@ -3,7 +3,7 @@ from wagtail.images.formats import Format, register_image_format, unregister_ima
 ##Note, update img-responsive to img-fluid 
 
 unregister_image_format('fullwidth')
-register_image_format(Format('fullwidth', 'Full width', 'richtext-image img-fluid full-width', 'width-800'))
+register_image_format(Format('fullwidth', 'Full width', 'richtext-image img-fluid full-width', 'width-700'))
 
 unregister_image_format('left')
 register_image_format(Format('left', 'Left-aligned', 'richtext-image img-fluid left', 'width-500'))  
