64309a32c87dd4cd2c951c4c382fdd837e8dc368
sewphl <37678331+sewphl@users.noreply.github.com>
2023-02-09 18:34:48 -0500

    migrate sqlite to postgres db

--- vcsc/settings/base.py
+++ vcsc/settings/base.py
@@ -99,12 +99,24 @@ WSGI_APPLICATION = "vcsc.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#    }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbpsql',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
