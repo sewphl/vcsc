c560944956f2582cdd4aed28c33a3ca2924cde67
sewphl <37678331+sewphl@users.noreply.github.com>
2023-04-07 10:24:07 -0400

    update base.py

--- vcsc/settings/base.py
+++ vcsc/settings/base.py
@@ -127,16 +127,16 @@ WSGI_APPLICATION = "vcsc.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbpsql',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '', #localhost
-        'PORT': '',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'dbpsql',
+#        'USER': os.getenv('DB_USER'),
+#        'PASSWORD': os.getenv('DB_PASSWORD'),
+#        'HOST': '', #localhost
+#        'PORT': '',
+#    }
+#}
 
 
 
