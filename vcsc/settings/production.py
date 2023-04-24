f17a7dfd45edd338bfa9d19c1151382c598243f6
sewphl <37678331+sewphl@users.noreply.github.com>
2023-04-24 14:41:16 -0400

    update production and dev files for heroku; add Procfile

--- vcsc/settings/production.py
+++ vcsc/settings/production.py
@@ -17,8 +17,17 @@ CACHES = {
 
 DEBUG = False
 
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Allow all host headers for now (TODO: Add correct URL and IP address and remove *)
 ALLOWED_HOSTS = ['*']
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 DATABASES = {
     'default': {
