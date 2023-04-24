d5999abc38a8c65c471a73b7a39ea66787d92aaf
sewphl <37678331+sewphl@users.noreply.github.com>
2023-04-24 17:59:21 -0400

    update base.py

--- vcsc/settings/production.py
+++ vcsc/settings/production.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['*']
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+##DATABASES['default'] =  dj_database_url.config()
 
 DATABASES = {
     'default': {
