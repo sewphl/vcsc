6d0f54cfdb81b1871657ce5c92d44455bda6065c
sewphl <37678331+sewphl@users.noreply.github.com>
2023-06-12 17:38:29 -0400

    update base.py, change STATIC_ROOT path and STATIC_URL

--- vcsc/settings/base.py
+++ vcsc/settings/base.py
@@ -211,8 +211,10 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/4.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
+##STATIC_ROOT = os.path.join(BASE_DIR, "static")
+##STATIC_URL = "/static/"
+STATIC_URL = '/vcsc/static/'
+STATIC_ROOT = '/vcsc/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
