faf7e8e8e29869f5d6721728b90286c4304ed7fb
sewphl <37678331+sewphl@users.noreply.github.com>
2023-06-16 12:46:57 -0400

    small update for heroku push

--- vcsc/settings/base.py
+++ vcsc/settings/base.py
@@ -266,8 +266,6 @@ GS_DEFAULT_ACL = "publicRead"
 #storage_client = storage.Client(
 #    project=gcp_project, credentials=credentials)
 
-
-
 # use service_account to generate credentials object
 # my_credentials = service_account.Credentials.from_service_account_info(CREDENTIALS) 
 
